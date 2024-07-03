c06d95a2eff59479439622e123d7846a6f973778
Gregory Pereverzev <boot762@gmail.com>
2024-07-03 15:27:09 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script></script>
+
+<template>
+  <h1>Hello</h1>
+  <div class="test-block">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora magnam minus, molestias odit
+    officia quidem corrupti architecto molestiae blanditiis doloremque eos reiciendis cum a modi sed
+    voluptatum harum alias sequi.
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+  border: 1px solid #000;
+}
+div {
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
